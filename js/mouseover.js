a97ea08305c5b77557c5ecb18921aa8fba82112f
Cory Jbara <cjbara@nd.edu>
2015-07-09 01:06:55 -0500

    Added text below contactme buttons

---
+++ js/mouseover.js
@@ -0,0 +1,31 @@
+
+$(document).ready(function() {
+	//This function makes the greyed out logos fade when hovered over
+	$('.connect-button').hover(
+       function(){ 
+       		$(this).css({
+   				'-webkit-filter': 'grayscale(0%)',
+				'height': '95px'
+       		});
+
+       		var id = $(this).attr('id');
+       		console.log(id+'.text');
+
+       		$('#'+id+'.text').css({
+       			'display': 'block'
+       		});
+       	}, function(){ 
+       		$(this).css({
+       			'-webkit-filter': 'grayscale(100%)',
+       			'height': '75px'
+       		});
+
+       		var id = $(this).attr('id');
+       		console.log(id+'.text');
+
+       		$('#'+id+'.text').css({
+       			'display': 'none'
+       		});
+       	}
+	);
+});
