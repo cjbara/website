0352bde91fe944dde9b0be45a052b456d479b99d
Cory Jbara <cjbara@nd.edu>
2015-07-14 19:24:02 -0500

    New Navbar, title page, background images

--- js/mouseover.js
+++ js/mouseover.js
@@ -28,4 +28,12 @@ $(document).ready(function() {
                      });
               }
        );
+
+       $(".nav a").on("click", function(){
+              $(".nav").find(".active").removeClass("active");
+              $(this).parent().addClass("active");
+       });
+       $(".navbar-header a").on("click", function(){
+              $(".nav").find(".active").removeClass("active");
+       });
 });
